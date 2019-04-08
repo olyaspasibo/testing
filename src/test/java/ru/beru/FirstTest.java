5ac3c2700e2bce13b5f08a3de3af653233a3dc37
pashevskiy <40821533+pashevskiy@users.noreply.github.com>
2019-04-08 21:46:45 +0400

    FirstTest issues
    please check the comments

--- src/test/java/ru/beru/FirstTest.java
+++ src/test/java/ru/beru/FirstTest.java
@@ -23,11 +23,13 @@ public class FirstTest extends WebDriverSettings {
         WebDriverWait wait = new WebDriverWait(driver, 8);
 
         //Authorize in the website
-        logIn("o.spasibo2016", "wegtov-gezwa3-gasfeN");
+        logIn("o.spasibo2016", "wegtov-gezwa3-gasfeN");  //Pavel: move this values into constants
 
         //Go to personal account
         WebElement headerItem = driver.findElement(By.className("header2__nav"));
-        takeScreenShot(headerItem);
+        takeScreenShot(headerItem);  //Pavel: you have to use 2 steps (taking screenshot and clicking element) instead of simple click.
+        // therefore you should create seperate function like "makeSreenshotAndClick" to avoid code duplitates
+        // also you can use custom webdriver instance with overrided  functions Click() and getAttribute();
         headerItem.click();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class=\"header2__nav\"]" +
